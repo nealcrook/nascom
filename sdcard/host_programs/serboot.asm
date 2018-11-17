fe3f390cbbff69b1264b8b4e497990beb6cbff7a
nealcrook <neal@pippaluk.org.uk>
2018-11-17 18:25:53 +0000

    Fixed bug where 1-character commands were not sent. Fixed bug where relocation always assumed source was at 0x0c80. Reformatted comments.

--- sdcard/host_programs/serboot.asm
+++ sdcard/host_programs/serboot.asm
@@ -1,69 +1,55 @@
-;;; Boot/command-line Utility for nascom_sdcard
-;;; serial interface
+;;; Boot/command-line Utility for nascom_sdcard serial interface
 ;;;
 ;;; https://github.com/nealcrook/nascom
 ;;;
-;;; This is the default program loaded across the serial
-;;; interface after boot. It is loaded using the "R"
-;;; command and is relocatable code so that it can be put
-;;; in any convenient location.
+;;; This is the default program loaded across the serial interface after boot.
+;;; It is loaded using the "R" command and is relocatable code so that it can
+;;; be put in any convenient location.
 ;;;
-;;; It provides a prompt and command loop and acts as a
-;;; console, relaying commands to the nascom_sdcard and
-;;; reporting responses.
+;;; It provides a prompt and command loop and acts as a console, relaying
+;;; commands to the nascom_sdcard and reporting responses.
 ;;;
-;;; By default it is assembled to load at location 0c80
-;;; because there is RAM there on NASCOM 1 and NASCOM 2
-;;; systems.
+;;; By default it is assembled to load at location 0c80 because there is RAM
+;;; there on NASCOM 1 and NASCOM 2 systems.
 ;;;
-;;; The command loop provides a mechanism whereby this code
-;;; relocates itself and continues to run, from the new
-;;; location. It will relocate to any non-overlapping
-;;; address.
+;;; The command loop provides a mechanism whereby this code can relocate
+;;; itself and continues to run, from the new location. It will relocate to
+;;; any non-overlapping address.
 ;;;
 ;;; When executed, it responds with a command prompt:
 ;;;
 ;;; SDcard>
 ;;;
-;;; Every command is sent directly to the nascom_sdcard
-;;; hardware across the serial interface. Type "." to exit
-;;; the command loop (either at the end of a line or on a
-;;; line by itself).
+;;; Every command is sent directly to the nascom_sdcard hardware across the
+;;; serial interface. Type "." to exit the command loop (either at the end
+;;; of a line or on a line by itself).
 ;;;
-;;; The command set is documented elsewhere (it is not
-;;; handled by this program).
+;;; The command set is documented elsewhere (it is not handled by this program).
 ;;;
 ;;; Configuration
 ;;; -------------
 ;;;
-;;; The NASCOM should be configured for cassette
-;;; operation with 1 stop bit. Various baud rates are
-;;; supported. The baud rate is set by a jumper on the
-;;; nascom_sdcard, and a configuration file on the SDcard
-;;; determines the baud rates associated with the jumper
-;;; settings.
+;;; The NASCOM should be configured for cassette operation with 1 stop bit.
+;;; Various baud rates are supported. The baud rate is set by a jumper on the
+;;; nascom_sdcard, and a configuration file on the SDcard determines the baud
+;;; rates associated with the jumper settings.
 ;;;
 ;;; Protocol
 ;;; --------
 ;;;
-;;; There is a command loop. When RETURN is pressed, the
-;;; command is send on the serial interface. The command
-;;; is the whole line upto the last non-blank or dot character.
-;;; The line is terminated with a NUL (0x00).
+;;; There is a command loop. When RETURN is pressed, the command is send on the
+;;; serial interface. The command is the whole line upto the last non-blank or
+;;; dot character. The line is terminated with a NUL (0x00).
 ;;;
 ;;; nascom_sdcard responds with one of three codes:
 ;;; RSDONE - command complete. No response.
-;;; RSMOVE hh ll - relocate the address given by following
-;;; two bytes
+;;; RSMOVE hh ll - relocate the address given by following two bytes
 ;;; RSMSG - print NUL-terminated text.
 ;;;
-;;; This code remembers whether the last command was terminated
-;;; with a . or not. If no . then the prompt is displayed
-;;; for another command. Otherwise, the program returns to
-;;; NAS-SYS.
-;;; A blank line results in a new prompt with no communication
-;;; with nascom_sdcard.
-;;;
+;;; This code remembers whether the last command was terminated with a . or not.
+;;; If no . then the prompt is displayed for another command. Otherwise, the
+;;; program returns to NAS-SYS.
+;;; A blank line results in a new prompt with no communication with nascom_sdcard.
 
 START:        EQU     $0c80
 
@@ -119,14 +105,13 @@ find:   dec     hl              ;move back by one character
 
 nodot:  push    af              ;save Z. Later, exit if Z
 
-;;; now, DE= 1st char of command, HL= last char of command. BUT, if the user
-;;; has back-spaced over the prompt, could have HL < DE. Cope with this by
-;;; ignoring the line and re-issuing the prompt.
+;;; now, DE= 1st char of command, HL= last char of command. If the line is blank
+;;; or the user has back-spaced over the prompt, will have HL < DE. Respond to
+;;; this by ignoring the line and re-issuing the prompt.
 
         or      a               ;clear carry
         sbc     hl,de
-        jr      z, done         ;blank line. Clean the stack and repeat prompt
-        jr      c, done         ;corrupted prompt. Clean the stack and repeat prompt
+        jr      c, done         ;blank/corrupted prompt. Clear stack and loop
 
         ld      b, l            ;count is <(48-PRLEN) so fits in 8 bits.
         inc     b               ;length of command
@@ -140,53 +125,61 @@ send:   ld      a,(hl)
         inc     hl
         djnz    send
 
-;;; nul-terminated
+;;; nul-terminate
         xor     a
         SCAL    ZSRLX
 
-;;; wait for response
+;;; get response
 eol:    RST     RIN
         cp      RSDONE
         jr      z, done         ;ready for next command, if any
         cp      RSMSG
         jr      z, prmsg
         cp      RSMOVE
-        jr      nz, fatal       ;something's wrong
+        jr      z, move
+;;; fatal error: print message and exit
+        SCAL    ZERRM
+        SCAL    ZMRET
+
 
 ;;; RSMOVE: get new address
+;;; first, find where current location in memory of START
+move:   call    move2
+move2:  ld      de,move2 - START;offset from start
+        pop     hl
+        or      a
+        sbc     hl,de           ;HL=current start
+
         RST     RIN
         ld      e, a            ;low byte
         RST     RIN
-        ld      d, a            ;high byte
+        ld      d, a            ;DE=destination
 
         pop     af              ;recover Z flag
         push    de              ;where to restart
-        ld      hl, START       ;current start
-        ld      bc, END-START+1 ;image size
+        ld      bc, END-START+1 ;BC=image size
         ldir                    ;move ourself
 
-        ;; AF (and so Z) was not affected by ldir
-        jr      z, exit         ;quit using current code
+;;; AF (and so Z) was not affected by ldir. Z tells us whether
+;;; to quit or whether to go back for another command. Stack is empty.
 
-;;; restart from the code in its new location, and get a new command
-        ret
+        jr      z, exit         ;quit using code at current location
+        ret                     ;jump to start of code at new location
 
-;;; print null-terminated string from sdcard
+
+;;; RSMSG: print null-terminated string from sdcard
 prmsg:  RST     RIN
         or      a               ;is it NUL?
         jr      z, done         ;yes; ready for next command, if any
         rst     ROUT
         jr      prmsg           ;carry on with string
 
-;;; Recover Z and either exit or get the next command
+;;; RSDONE: recover Z and either exit or get the next command
 done:   pop     af
         jr      nz, newcmd
 
 ;;; finished
 exit:   SCAL    ZMRET
 
-;;; fatal error
-fatal:  SCAL    ZERRM
-        SCAL    ZMRET
 END:
 ;;; end
